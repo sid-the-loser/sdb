a4b637c17de32ee94886d85ff198fd445206b680
sid-the-loser <69863204+sid-the-loser@users.noreply.github.com>
2021-12-04 07:08:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import discord
+from random import randint, choice
+
+help_text = discord.Embed(title="Help", url="", description=open("help.txt").read())
+about_text = discord.Embed(title="About", url="https://sidtheloser.netlify.app/", description=open("about.txt").read())
+log_file = open("log.txt", "w").close()
+
+TOKEN = open("SDB.txt", "r").readline()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"{client.user} started session!")
+    await client.change_presence(activity=discord.Game("good"), status=discord.Status.online)
+
+@client.event
+async def on_message(message):
+    string = message.content.lower()
+    if not message.author.bot:
+        if string.startswith("~"):
+            string = string.lstrip("~")
+        
+            if string.startswith("hello"):
+                await message.channel.send(f"Hello {message.author}!")
+
+            elif string.startswith("coin"):
+                await message.channel.send(f"{message.author} toseed a coin and got: {choice(['heads', 'tails'])}")
+
+            elif string.startswith("dice"):
+                await message.channel.send(f"{message.author} rolled a dice and got: {randint(1, 6)}")
+
+            elif string.startswith("about"):
+                await message.channel.send(embed=about_text)
+
+            elif string.startswith("say "):
+                _ = (message.content.lstrip("~")).lstrip("say ")
+                await message.channel.send(_)
+
+            elif string.startswith("status "):
+                await client.change_presence(activity=discord.Game(message.content.lstrip("~status ")))
+
+            else:
+                await message.channel.send(embed=help_text)
+
+            print(f"{message.author}, used command {message.content}")
+            log_file = open("log.txt", "a")
+            log_file.write(f"{message.author}, used command {message.content}\n")
+            log_file.close()
+
+client.run(TOKEN)
