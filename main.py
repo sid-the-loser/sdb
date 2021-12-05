0f1d197fd4f21240910a0fd8a90458b97de8c8f0
sid-the-loser <69863204+sid-the-loser@users.noreply.github.com>
2021-12-05 10:24:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -21,23 +21,23 @@ async def on_message(message):
             string = string.lstrip("~")
         
             if string.startswith("hello"):
-                await message.channel.send(f"Hello {message.author}!")
+                await message.channel.send(f"Hello {message.author.name}!")
 
             elif string.startswith("coin"):
-                await message.channel.send(f"{message.author} toseed a coin and got: {choice(['heads', 'tails'])}")
+                await message.channel.send(f"{message.author.name} toseed a coin and got: {choice(['heads', 'tails'])}")
 
             elif string.startswith("dice"):
-                await message.channel.send(f"{message.author} rolled a dice and got: {randint(1, 6)}")
+                await message.channel.send(f"{message.author.name} rolled a dice and got: {randint(1, 6)}")
 
             elif string.startswith("about"):
                 await message.channel.send(embed=about_text)
 
             elif string.startswith("say "):
-                _ = (message.content.lstrip("~")).lstrip("say ")
-                await message.channel.send(_)
+                await message.channel.send(message.content.lstrip("~say "))
 
             elif string.startswith("status "):
                 await client.change_presence(activity=discord.Game(message.content.lstrip("~status ")))
+                await message.channel.send(f"{message.author.name} changed the status of the bot to {message.content.lstrip('~status ')}")
 
             else:
                 await message.channel.send(embed=help_text)
