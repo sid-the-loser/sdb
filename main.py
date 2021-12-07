3a56d44848b84761a893c79cca1aea9e99ca9ad4
sid-the-loser <69863204+sid-the-loser@users.noreply.github.com>
2021-12-07 19:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import discord
 from random import randint, choice
+from json import load, dump
 
 help_text = discord.Embed(title="Help", url="", description=open("help.txt").read())
 about_text = discord.Embed(title="About", url="https://sidtheloser.netlify.app/", description=open("about.txt").read())
 log_file = open("log.txt", "w").close()
+status = open("status.txt", encoding="utf-8").readline()
+game_data = load(open("game.json"))
 
 TOKEN = open("SDB.txt", "r").readline()
 client = discord.Client()
@@ -11,10 +14,11 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f"{client.user} started session!")
-    await client.change_presence(activity=discord.Game("good"), status=discord.Status.online)
+    await client.change_presence(activity=discord.Game(status), status=discord.Status.online)
 
 @client.event
 async def on_message(message):
+    global game_data
     string = message.content.lower()
     if not message.author.bot:
         if string.startswith("~"):
@@ -37,14 +41,33 @@ async def on_message(message):
 
             elif string.startswith("status "):
                 await client.change_presence(activity=discord.Game(message.content.lstrip("~status ")))
-                await message.channel.send(f"{message.author.name} changed the status of the bot to {message.content.lstrip('~status ')}")
+                _ = open("status.txt", "w", encoding="utf-8"); _.write(message.content.lstrip("~status ")); _.close; del _
+                await message.channel.send(f"{message.author.name} changed the status of the bot to: {message.content.lstrip('~status ')}")
+            
+            elif string.startswith("mine"):
+                if not str(message.author.id) in game_data:
+                    await message.channel.send("As you are playing this game for the first time, let me explain what you are gonna get. This is a game about mining coins and you have 1 in a million chance of getting a coin. The more the coins you have, the more you can show off.")
+                    game_data[str(message.author.id)] = 0
+                if randint(1, 1000000) == 1:
+                    game_data[str(message.author.id)] += 1
+                    await message.channel.send("Congratulations, you just got a coin!")
+                else:
+                    await message.channel.send("So sed, you got nothin.")
+
+            elif string.startswith("bal"):
+                if not str(message.author.id) in game_data:
+                    await message.channel.send("As you are playing this game for the first time, let me explain what you are gonna get. This is a game about mining coins and you have 1 in a million chance of getting a coin. The more the coins you have, the more you can show off.")
+                    game_data[str(message.author.id)] = 0
+                
+                await message.channel.send(f"{message.author.name}'s wallet has: {game_data[str(message.author.id)]} coins.")
 
             else:
                 await message.channel.send(embed=help_text)
 
             print(f"{message.author}, used command {message.content}")
-            log_file = open("log.txt", "a")
+            log_file = open("log.txt", "a", encoding="utf-8")
             log_file.write(f"{message.author}, used command {message.content}\n")
             log_file.close()
+            dump(game_data, open("game.json", "w"))
 
 client.run(TOKEN)
